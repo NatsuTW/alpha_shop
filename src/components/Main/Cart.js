c044f636df2e2553367b6dbcb2f19ea28b1284ea
Natsu <natsu.tw0919@gmail.com>
2022-11-19 09:14:14 +0900

    Delete the setState and the function for calculating total price because they are lift to CartContext and then obtain the data of them through useContext.

--- src/components/Main/Cart.js
+++ src/components/Main/Cart.js
@@ -1,48 +1,40 @@
-import { useState, useContext } from 'react';
-import { CartContext } from './Cart/CartContext';
+import { useContext } from 'react';
+import { CartContext } from '../CartContext';
 import { ReactComponent as PlusBtn } from '../../icons/plus.svg'
 import { ReactComponent as MinusBtn } from '../../icons/minus.svg'
 
 import styles from "./Cart.module.css"
 
-function ProductList({handlePlus, handleMinus, products}) {
-  
+function ProductList({ handlePlus, handleMinus, products }) {
+
   const productItems = products.map(product =>
-    <div className = {styles.product__container} key={product.id}>
+    <div className={styles.product__container} key={product.id}>
       <img className={styles.product__image} src={product.img} alt={product.name} />
       <div className={styles.product__info}>
         <div className={styles.product__name}>{product.name}</div>
         <div className={styles.product__control__container}>
           <div className={styles.product__control}>
-            <PlusBtn 
+            <PlusBtn
               className={styles.product__control__btn}
-              onClick={()=> handlePlus(product.id)}/>
-              <span className={styles.product__count}>{product.quantity}</span>
+              onClick={() => handlePlus(product.id)} />
+            <span className={styles.product__count}>{product.quantity}</span>
             <MinusBtn
               className={styles.product__control__btn}
-              onClick={() => handleMinus(product.id)}/>
+              onClick={() => handleMinus(product.id)} />
           </div>
         </div>
         <div className={styles.product__price}>${product.price}</div>
       </div>
-    </div> 
-    )
+    </div>
+  )
   return (
     <section className={styles.product__list}>{productItems}</section>
   )
 }
 
 export default function Cart() {
-  const products = useContext(CartContext)
-  const [currentProducts, setCurrentProducts] = useState(products)
-
-  function isTotal() {
-    let total = null;
-    currentProducts.map(product => total = total + product.price * product.quantity)
-    
-    return total
-  }
-
+  const { currentProducts, setCurrentProducts, total } = useContext(CartContext)
+ 
   function handleClickPlus(id) {
     setCurrentProducts(currentProducts.map(product => {
       if (product.id === id) {
@@ -66,27 +58,25 @@ export default function Cart() {
       } else {
         return product
       }
-    }) 
+    })
     setCurrentProducts(newCurrentProducts.filter(product => product.quantity !== 0))
   }
 
-  return(
-      <section className={styles.cart__container}>
-          <h3 className={styles.cart__title}>購物籃</h3>
-          <ProductList
-            products={currentProducts}
-            handlePlus={handleClickPlus}
-            handleMinus={handleClickMinus}/>
-          <section className={styles.cart__info}>
-            <div className={styles.cart__text}>運費</div>
-            <div className={styles.cart__price}>免費</div>
-          </section>
-          <section className={styles.cart__info}>
-            <div className={styles.cart__text}>小計</div>
-          <div className={styles.cart__price}>${isTotal()}</div>
-          </section>
+  return (
+    <section className={styles.cart__container}>
+      <h3 className={styles.cart__title}>購物籃</h3>
+      <ProductList
+        products={currentProducts}
+        handlePlus={handleClickPlus}
+        handleMinus={handleClickMinus} />
+      <section className={styles.cart__info}>
+        <div className={styles.cart__text}>運費</div>
+        <div className={styles.cart__price}>免費</div>
       </section>
+      <section className={styles.cart__info}>
+        <div className={styles.cart__text}>小計</div>
+        <div className={styles.cart__price}>{total()}</div>
+      </section>
+    </section>
   )
 }
-
-
