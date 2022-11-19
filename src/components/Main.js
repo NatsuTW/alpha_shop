b51674b0b698f3363011bc0bf877c445742c0da5
Natsu <natsu.tw0919@gmail.com>
2022-11-19 09:33:17 +0900

    Import FormContextProvider and wrap components of Progress and Cart in order to pass the state of form data and handler function to them.

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,6 +1,7 @@
 import Progress from './Main/Progress'
 import Cart from './Main/Cart'
 import { CartContextProvider }from './CartContext'
+import { FormContextProvider } from './FormContext'
 
 import styles from "./Main.module.css"
 
@@ -8,10 +9,12 @@ export default function Main() {
   return(
     <main className={styles.site__main}>
       <div className={styles.main__container}>
-        <CartContextProvider>
-          <Progress />
-          <Cart />
-        </CartContextProvider>
+        <FormContextProvider>
+          <CartContextProvider>
+            <Progress />
+            <Cart />
+          </CartContextProvider>
+        </FormContextProvider>
       </div>
     </main>
   )
