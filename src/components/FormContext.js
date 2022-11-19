c270542f77f0afd76d1ae51c1c361b5a9a843b09
Natsu <natsu.tw0919@gmail.com>
2022-11-19 09:22:01 +0900

    Add FormContextProvider component to store the event handler function and state of form info in order to obtain and store form data from step three when there is any input entered.

---
+++ src/components/FormContext.js
@@ -0,0 +1,27 @@
+import { createContext } from 'react';
+import { useState } from 'react';
+
+const info = {
+  cardName: '',
+  cardNum: '',
+  cardDate: '',
+  cardCvc: ''
+}
+
+const FormContext = createContext()
+
+function FormContextProvider({ children }) {
+  const [formInfo, setFormInfo] = useState(info)
+
+  function handleInputChange(e) {
+    setFormInfo({ ...formInfo, [e.target.name]: e.target.value })
+  }
+
+  return (
+    <FormContext.Provider value={{ formInfo, setFormInfo, handleInputChange }}>
+      {children}
+    </FormContext.Provider>
+  )
+}
+
+export { FormContext, FormContextProvider }
