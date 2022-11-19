62a1b8eb42fb11b3c7adce034f564f06c9409e71
Natsu <natsu.tw0919@gmail.com>
2022-11-19 09:50:52 +0900

    Revise import due to file location change.

--- src/components/Main/Progress/StepForm.js
+++ src/components/Main/Progress/StepForm.js
@@ -1,13 +1,13 @@
-import StepOne from "./StepOne";
-import StepTwo from "./StepTwo";
-import StepThree from "./StepThree";
+import StepOne from "./StepForm/StepOne";
+import StepTwo from "./StepForm/StepTwo";
+import StepThree from "./StepForm/StepThree";
 
-export default function StepForm({currentStep}) {
-  return(
+export default function StepForm({ currentStep }) {
+  return (
     <>
       {currentStep === 1 && <StepOne />}
       {currentStep === 2 && <StepTwo />}
-      {currentStep === 3 && <StepThree/>}
+      {currentStep === 3 && <StepThree />}
     </>
-  )   
+  )
 }
